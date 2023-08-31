601ca8bb1ee666019f7da69c95e6eb284603e0a2
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-08-31 23:28:52 +0700

    fix: add button name for seo

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -18,6 +18,7 @@ export default function ThemeSwitch() {
     case 'light':
       return (
         <Button
+          name='theme-switch'
           className='mr-2'
           size={'icon'}
           variant={'ghost'}
@@ -30,6 +31,7 @@ export default function ThemeSwitch() {
     case 'dark':
       return (
         <Button
+          name='theme-switch'
           className='mr-2'
           size={'icon'}
           variant={'ghost'}
